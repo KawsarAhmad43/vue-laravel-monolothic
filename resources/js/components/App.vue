ea04076dfa98b8c9844298e4994949c2af8da2f5
KawsarAhmad43 <kaleon299@gmail.com>
2024-08-03 04:05:09 +0600

    initial commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Hello from Vue.js!</h1>
+      <button @click="showMessage">Click me</button>
+      <p v-if="message">{{ message }}</p>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        message: ''
+      };
+    },
+    methods: {
+      showMessage() {
+        this.message = 'Hello from Vue!';
+      }
+    }
+  };
+  </script>
+
+  <style scoped>
+  /* Scoped styles for this component */
+  </style>
